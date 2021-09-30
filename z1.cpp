b68a776fcf2a3fb6d45be86ab754045e25e8258a
phrnb <90862775+phrnb@users.noreply.github.com>
2021-10-01 00:07:50 +0300

    Create z1.cpp

---
+++ z1.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	system("chcp 1251");
+	system("cls");
+	int i = 0;
+	char number[100];
+	cout << "Enter the number: ";
+	cin >> number;
+	while (number[i] != '\0')
+	{
+		cout << number[i] << endl;
+		i++;
+	}
+	system("pause");
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
